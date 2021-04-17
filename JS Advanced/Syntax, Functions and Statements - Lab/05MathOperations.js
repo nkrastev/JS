d7d25cec299b3228c05398b40e1e69837e42db6b
nkrastev <nikolay.krystev@gmail.com>
2021-04-17 13:07:37 +0300

    fix

--- JS Advanced/Syntax, Functions and Statements - Lab/05MathOperations.js
+++ JS Advanced/Syntax, Functions and Statements - Lab/05MathOperations.js
@@ -1,3 +1,16 @@
 function mathOperation(num1, num2, operation){
-        
+    if(operation==='+'){
+        console.log(num1+num2)
+    }else if(operation==='-'){
+        console.log(num1-num2)        
+    }else if(operation==='*'){
+        console.log(num1*num2)
+    }else if(operation==='/'){
+        console.log(num1/num2)
+    }else if(operation==='%'){
+        console.log(num1 % num2)
+    }else if(operation==='**'){
+        console.log(num1**num2)
+    }    
+
 }
