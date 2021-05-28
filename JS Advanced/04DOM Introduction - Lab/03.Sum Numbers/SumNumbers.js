b2de78ed8f2a8946ebf2ead242b8e50976dc6be6
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-05-28 13:00:18 +0300

    DOM

---
+++ JS Advanced/04DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,8 @@
+function calc() {
+    // TODO: sum = num1 + num2
+    let firstNum=Number(document.getElementById('num1').value);
+    let secondNum=Number(document.getElementById('num2').value);
+
+    document.getElementById('sum').value=firstNum+secondNum;
+    
+}
