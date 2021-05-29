5d09ed431cb6e543c5c5134bfbcd04ae5f0e03b7
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-05-29 16:55:20 +0300

    Create 01Area and Volume Calculator.js

---
+++ JS Advanced/06Advanced Functions - Lab/01Area and Volume Calculator.js
@@ -0,0 +1,21 @@
+function area() {
+    return Math.abs(this.x * this.y);
+};
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+};
+
+
+function solve(area, vol, input) {
+    let objects = JSON.parse(input);
+    function calc(obj) {
+        let areaObj = Math.abs(area.call(obj));
+        let volumeObj = Math.abs(vol.call(obj));
+        return { area: areaObj, volume: volumeObj }
+      }
+      return objects.map(calc);
+    
+}
+
+
+console.log(solve(area, vol, '[{"x":"1","y":"2","z":"10"},{"x":"7","y":"7","z":"10"},{"x":"5","y":"2","z":"10"}]'));
