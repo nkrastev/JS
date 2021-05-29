646e386d9486bfe891fd8ccd9d38fdd639e21b27
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-05-29 18:15:13 +0300

    Added Test data

--- JS Advanced/06Advanced Functions - Lab/05Command Processor.js
+++ JS Advanced/06Advanced Functions - Lab/05Command Processor.js
@@ -7,3 +7,11 @@ function solution() {
             print: () => console.log(str)
         }
 }
+
+let firstZeroTest = solution();
+
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
