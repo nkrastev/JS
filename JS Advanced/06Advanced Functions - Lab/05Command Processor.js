cb5bf7b938d4a1e4577161d216a50d94306bcf7f
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-05-29 18:14:20 +0300

    Create 05Command Processor.js

---
+++ JS Advanced/06Advanced Functions - Lab/05Command Processor.js
@@ -0,0 +1,9 @@
+function solution() {
+        let str = '';
+        return {
+            append: (s) => str += s,
+            removeStart: (n) => str = str.substring(n),
+            removeEnd: (n) => str = str.substring(0, str.length - n),
+            print: () => console.log(str)
+        }
+}
