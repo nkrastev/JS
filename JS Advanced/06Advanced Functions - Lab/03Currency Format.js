ca6f0b981541cd7dd64d290cda29af8b487870ea
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-05-29 17:19:26 +0300

    Create 03Currency Format.js

---
+++ JS Advanced/06Advanced Functions - Lab/03Currency Format.js
@@ -0,0 +1,15 @@
+function currencyFormatter(separator, symbol, symbolFirst, value) {
+    let result = Math.trunc(value) + separator;
+    result += value.toFixed(2).substr(-2,2);
+    if (symbolFirst) return symbol + ' ' + result;
+    else return result + ' ' + symbol;
+}
+
+function createFormatter(param1, param2, param3, currencyFormatter) {
+    return currencyFormatter.bind(null, param1, param2, param3);
+}
+
+let dollarFormatter = createFormatter(',', '$', true, currencyFormatter);
+console.log(dollarFormatter(5345));   // $ 5345,00
+console.log(dollarFormatter(3.1429)); // $ 3,14
+console.log(dollarFormatter(2.709));  // $ 2,71
