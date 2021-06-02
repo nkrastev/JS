51ce607d3ed2e1c70b26758bb251834f4897424d
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-06-02 13:58:00 +0300

    DOM

---
+++ JS Advanced/04DOM Introduction - Exercise/01. Subtraction/subtract.js
@@ -0,0 +1,6 @@
+function subtract() {
+    let firstNum=Number(document.getElementById('firstNumber').value);
+    let secondNum=Number(document.getElementById('secondNumber').value);
+
+    document.getElementById('result').textContent=firstNum-secondNum;
+}
