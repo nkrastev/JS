3d1bc9fd7464b5579b35cd32c86d25d2fc539d06
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-06-24 19:52:06 +0300

    Update app.js

--- JS Advanced/10Exams/19nov2019-04Darts/app.js
+++ JS Advanced/10Exams/19nov2019-04Darts/app.js
@@ -104,13 +104,56 @@ function dart(){
 		if (home>=100) {
 			document.querySelector('#Home > p:nth-child(2)').style.background='green';
 			document.querySelector('#Away > p:nth-child(2)').style.background='red';
-			
+			exit();
 		}
 		if (away>=100) {
 			document.querySelector('#Home > p:nth-child(2)').style.background='red';
 			document.querySelector('#Away > p:nth-child(2)').style.background='green';						
-			
+			exit();
 		}
 		
 	}	
+}
+
+
+
+function exit( status ) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir (http://brettz9.blogspot.com)
+    // +      input by: Paul
+    // +   bugfixed by: Hyam Singer (http://www.impact-computing.com/)
+    // +   improved by: Philip Peterson
+    // +   bugfixed by: Brett Zamir (http://brettz9.blogspot.com)
+    // %        note 1: Should be considered expirimental. Please comment on this function.
+    // *     example 1: exit();
+    // *     returns 1: null
+
+    var i;
+
+    if (typeof status === 'string') {
+        alert(status);
+    }
+
+    window.addEventListener('error', function (e) {e.preventDefault();e.stopPropagation();}, false);
+
+    var handlers = [
+        'copy', 'cut', 'paste',
+        'beforeunload', 'blur', 'change', 'click', 'contextmenu', 'dblclick', 'focus', 'keydown', 'keypress', 'keyup', 'mousedown', 'mousemove', 'mouseout', 'mouseover', 'mouseup', 'resize', 'scroll',
+        'DOMNodeInserted', 'DOMNodeRemoved', 'DOMNodeRemovedFromDocument', 'DOMNodeInsertedIntoDocument', 'DOMAttrModified', 'DOMCharacterDataModified', 'DOMElementNameChanged', 'DOMAttributeNameChanged', 'DOMActivate', 'DOMFocusIn', 'DOMFocusOut', 'online', 'offline', 'textInput',
+        'abort', 'close', 'dragdrop', 'load', 'paint', 'reset', 'select', 'submit', 'unload'
+    ];
+
+    function stopPropagation (e) {
+        e.stopPropagation();
+        // e.preventDefault(); // Stop for the form controls, etc., too?
+    }
+    for (i=0; i < handlers.length; i++) {
+        window.addEventListener(handlers[i], function (e) {stopPropagation(e);}, true);
+    }
+
+    if (window.stop) {
+        window.stop();
+    }
+
+    throw '';
 }
