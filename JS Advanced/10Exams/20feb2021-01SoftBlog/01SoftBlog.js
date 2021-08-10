3cef99e146354bba5ad7b454910212daefd140cd
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-08-10 16:31:26 +0300

    Refactor

--- JS Advanced/10Exams/20feb2021-01SoftBlog/01SoftBlog.js
+++ JS Advanced/10Exams/20feb2021-01SoftBlog/01SoftBlog.js
@@ -1,91 +1,72 @@
-function solve(){
+function solve() {
+   let postsSection=document.querySelector('body > div > div > main > section');
+   let createButton=document.querySelector('body > div > div > aside > section:nth-child(1) > form > button');
+   createButton.addEventListener('click', onClickAddPost);
 
-    let bntCreate=document.querySelector("button.create");
-    //bntCreate.type='button'; // type="button" prevent page from reloading !!!!    
-    bntCreate.addEventListener('click', onClickCreate);
-    
-    let positionToAddArticles=document.querySelector('main > section');
+   function onClickAddPost(event) {
+      event.preventDefault();
+      let author=document.querySelector('#creator').value;
+      let title=document.querySelector('#title').value;
+      let category=document.querySelector('#category').value;
+      let content=document.querySelector('#content').value;
+      //create item
+      let article=e('article',null,null);
+      let h1=e('h1',title,null);
+      
+      let p1=e('p','Category: ',null,null);
+      p1.appendChild(e('strong',category,null));
    
+   
+      let p2=e('p', 'Creator: ',null,null);
+      p2.appendChild(e('strong',author,null));
+   
+      let p3=e('p',content,null);
+      let divBtns=e('div',null,'buttons');
+      let btnDel=e('button','Delete','btn delete');
+      btnDel.addEventListener('click', onDeleteClick);
+      let btnArc=e('button','Archive','btn archive');
+      btnArc.addEventListener('click', onArchiveClick);
+   
+      divBtns.appendChild(btnDel);
+      divBtns.appendChild(btnArc);
+   
+      article.appendChild(h1);
+      article.appendChild(p1);
+      article.appendChild(p2);
+      article.appendChild(p3);
+      article.appendChild(divBtns);
+      document.querySelector('body > div > div > main > section').appendChild(article);
+   }
 
-    function onClickCreate(event){
-        //get data
-        event.preventDefault();
-        const creator=document.getElementById('creator').value;
-        const title=document.getElementById('title').value;
-        const category=document.getElementById('category').value;
-        const content=document.getElementById('content').value;
+   function onDeleteClick(event) {
+      event.preventDefault();
+      event.target.parentNode.parentNode.remove();
+   }
+   function onArchiveClick(event) {
+      event.preventDefault();
+      let elementForMovement=event.target.parentNode.parentNode.querySelector('h1').innerHTML;
+      event.target.parentNode.parentNode.remove();
+      let archiveList=document.querySelector('body > div > div > aside > section.archive-section > ol');
+      archiveList.appendChild(e('li',elementForMovement,null));
 
-        //create and append
-        
-        let newArticle = document.createElement("article");
-        
-        let newH1 = document.createElement("h1");        
-        newH1.textContent=title;
-        newArticle.appendChild(newH1);
+      //sorting stack overflow RULZ :)        
+      let ol = document.getElementsByTagName('ol')[0];
+      Array.from(ol.getElementsByTagName("li"))
+      .sort((a, b) => a.textContent.localeCompare(b.textContent))
+      .forEach(li => ol.appendChild(li));
+      
+   }
+   
+   //factory function, create DOM element
+   function e(type, content, className) {
+      const result = document.createElement(type);
+      result.textContent = content;
+      if (className) {
+         result.className = className;
+      }
+      return result;
+   }
 
-        let catParagraph=document.createElement("p");         
-        catParagraph.innerHTML='Category: <strong>'+category+'</strong>';
-        newArticle.appendChild(catParagraph);
-
-        let creatorParagraph=document.createElement("p"); 
-        creatorParagraph.innerHTML='Creator: <strong>'+creator+'</strong>';
-        newArticle.appendChild(creatorParagraph);
-
-        let descParagraph=document.createElement("p"); 
-        descParagraph.textContent=content;
-        newArticle.appendChild(descParagraph);
-
-        //buttons
-        let buttonsDiv= document.createElement("div");
-        buttonsDiv.classList.add('buttons');
-
-        //create button delete
-        let btnDelete=document.createElement('button');
-        btnDelete.classList.add('btn');
-        btnDelete.classList.add('delete');
-        //btnDelete.type='button';
-        btnDelete.innerText='Delete';
-        btnDelete.addEventListener('click', onClickDelete);
-        //add delete to div
-        buttonsDiv.appendChild(btnDelete);
-
-        //create button archive
-        let btnArchive=document.createElement('button');
-        btnArchive.classList.add('btn');
-        btnArchive.classList.add('archive');
-        //btnArchive.type='button';
-        btnArchive.innerText='Archive';
-        btnArchive.addEventListener('click', onClickArchive);
-        //add delete to div
-        buttonsDiv.appendChild(btnArchive);
-
-
-        //add buttons div to article
-        newArticle.appendChild(buttonsDiv);
-
-        //add article to document
-        positionToAddArticles.appendChild(newArticle);        
-    }
-
-    function onClickDelete(event){
-        event.target.parentNode.parentNode.remove();
-    }
-    function onClickArchive(event){
-        //get data
-        let liItem=document.createElement('li');
-        liItem.textContent=(event.target.parentNode.parentNode).getElementsByTagName('h1')[0].textContent
-        //delete article
-        event.target.parentNode.parentNode.remove();
-        //appent to list
-        let archiveData=document.querySelector('.archive-section>ol');        
-        archiveData.appendChild(liItem);
-
-        //sorting stack overflow RULZ :)        
-        let ol = document.getElementsByTagName('ol')[0];
-        Array.from(ol.getElementsByTagName("li"))
-        .sort((a, b) => a.textContent.localeCompare(b.textContent))
-        .forEach(li => ol.appendChild(li));
-    }
 }
 
 
