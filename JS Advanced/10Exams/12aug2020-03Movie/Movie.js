3d418a47e021cc5df722695b7a008834d4d08357
Nikolay Krastev <nkrastev@users.noreply.github.com>
2021-08-11 18:04:27 +0300

    One line remove from array of objects, sort ASC

--- JS Advanced/10Exams/12aug2020-03Movie/Movie.js
+++ JS Advanced/10Exams/12aug2020-03Movie/Movie.js
@@ -2,64 +2,53 @@ class Movie{
     constructor ( movieName, ticketPrice ){
         this.movieName=movieName;
         this.ticketPrice=Number(ticketPrice);
-        this.screenings=[];
-        
-        this.profit=0;
+        this.screenings=[]; //array of objects
+        this.totalProfit=0;
         this.totalSoldTickets=0;
     }
-
     newScreening(date, hall, description){
-        if (this.screenings.some(e=>e.date==date && e.hall==hall)) {
+        if (this.screenings.find(e=>e.date==date && e.hall==hall)) {
             throw new Error(`Sorry, ${hall} hall is not available on ${date}`);
         }
-        let newScreening={date:date, hall:hall, description:description};
-        this.screenings.push(newScreening);
+        this.screenings.push({date,hall,description});
         return `New screening of ${this.movieName} is added.`;
     }
-
-    endScreening(date, hall, soldTickets) {
-        if (!this.screenings.some(e=>e.date==date && e.hall==hall)) {
+    endScreening(date, hall, soldTickets){
+        if (!this.screenings.find(e=>e.date==date && e.hall==hall)) {
             throw new Error(`Sorry, there is no such screening for ${this.movieName} movie.`);
         }
-        let targetScreening=this.screenings.find(e=>e.date==date && e.hall==hall);
-        let targetIndex=this.screenings.indexOf( targetScreening);
-
-        this.screenings.splice(targetIndex, 1);
-
-        let currentProfit=soldTickets*this.ticketPrice;
-        this.profit+=currentProfit;
+        this.totalProfit+=this.ticketPrice*soldTickets;
         this.totalSoldTickets+=soldTickets;
-
-        return `${this.movieName} movie screening on ${date} in ${hall} hall has ended. Screening profit: ${currentProfit}`;
-
+        
+        this.screenings.splice(this.screenings.findIndex(e=>e.date==date && e.hall==hall), 1);
+        return `${this.movieName} movie screening on ${date} in ${hall} hall has ended. Screening profit: ${this.ticketPrice*soldTickets}`;
     }
-    toString (){
+    toString(){
         let result=`${this.movieName} full information:\n`;
-        result+=`Total profit: ${this.profit.toFixed(0)}$\n`;
-        result+=`Sold Tickets: ${this.totalSoldTickets}\n`;
+        result+=`Total profit: ${this.totalProfit.toFixed(0)}$\n`;
+        result+=`Sold Tickets: ${this.totalSoldTickets}\n`;        
         if (this.screenings.length>0) {
-            this.screenings.sort((a,b) => (a.hall > b.hall) ? 1 : ((b.hall > a.hall) ? -1 : 0));
             result+=`Remaining film screenings:\n`;
+            this.screenings.sort((a, b) => a.hall.localeCompare(b.hall));
             for (const item of this.screenings) {
                 result+=`${item.hall} - ${item.date} - ${item.description}\n`;
             }
         }else{
-            result+='No more screenings!';
+            result+=`No more screenings!`;
         }
         return result.trimEnd();
     }
 }
 
 let m = new Movie('Wonder Woman 1984', '10.00');
-console.log(m.newScreening('October 2, 2020', 'IMAX 3D', `3D`));
-console.log(m.newScreening('October 3, 2020', 'Main', `regular`));
-console.log(m.newScreening('October 4, 2020', 'IMAX 3D', `3D`));
-console.log(m.endScreening('October 2, 2020', 'IMAX 3D', 150));
-console.log(m.endScreening('October 3, 2020', 'Main', 78));
-console.log(m.toString());
+console.log(m.newScreening('1', 'A', `3D`));
+console.log(m.newScreening('1', 'B', `3D`));
+console.log(m.newScreening('1', 'D', `3D`));
+console.log(m.newScreening('1', 'C', `3D`));
 
-m.newScreening('October 4, 2020', '235', `regular`);
-m.newScreening('October 5, 2020', 'Main', `regular`);
-m.newScreening('October 3, 2020', '235', `regular`);
-m.newScreening('October 4, 2020', 'Main', `regular`);
+console.log(m.endScreening('1', 'A', 2));
+console.log(m.endScreening('1', 'B', 3));
+console.log(m.endScreening('1', 'C', 4));
+//console.log(m.endScreening('1', 'D', 5));
+
 console.log(m.toString());
