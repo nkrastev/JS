ee84d3f4ba2fb7c082d0424edce03cf26fd0c440
Nikolay Krastev <nikolay.krystev@gmail.com>
2022-06-14 19:31:42 +0300

    Initial commit

---
+++ React/Components/demo/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
